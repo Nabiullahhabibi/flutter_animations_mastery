10478be56289475bd49b912863a3c400089d42f4
Developer <nabiullahhabibikdr@gmail.com>
2026-08-18 11:53:06 +0430

    the rabbit loading animation added

---
+++ README.md
@@ -0,0 +1,5 @@
+# 🧬✨ Flutter Animation World ⚡️
+
+Welcome to **Flutter Animation World** — a 🔮 futuristic, high-performance Flutter showcase crafted by **Nabiullah Habibi**.
+
+This repo is a digital playground 🕹️ full of **jaw-dropping UI**, **smooth transitions**, and **cyber-cool animations** that push the boundaries of Flutter.
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animations/screens/home_screen.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FutureCore Codex',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF6C63FF),
+          secondary: const Color(0xFFFF6584),
+        ),
+        useMaterial3: true,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
