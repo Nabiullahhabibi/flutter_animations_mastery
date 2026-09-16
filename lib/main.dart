b11e2cecac14b7e5ab179cfdb911e255919356dd
Developer <nabiullahhabibikdr@gmail.com>
2026-09-16 16:03:05 +0430

    3d album animation added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animations/flutter_animations_mastery/animation_test.dart';
 import 'package:flutter_animations/screens/2-lamp_on_off_animaiton/lamp_on_off_screen.dart';
-import 'package:flutter_animations/screens/3-free_palestine_screen/deadline_animations_screens.dart';
+import 'package:flutter_animations/screens/4-3d_album_screen/3d_album_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // home: HomeScreen(),
-      home: PalestineFlagScreen(),
+      home: SplashScreen3D(),
     );
   }
 }
